2d86aae288006b97ffb7ab0d14858545e38291c2
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-08-12 12:07:56 +0200

    seed 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5, category: "belgian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4, category: "italian" }
+cocina =  { name: "Cocina", address: "Flagey", stars: 5, category: "italian" }
+mc_donalds =  { name: "McDonalds", address: "City 2", stars: 3, category: "belgian" }
+belgica =  { name: "Belgica", address: "Sablon", stars: 4, category: "belgian" }
+[ dishoom, pizza_east, cocina, mc_donalds, belgica ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
