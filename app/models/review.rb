8dc8fd0e265e9e3f2ab8e2ab63364f6f33785dfb
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-08-12 11:36:39 +0200

    models configured

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  RATING = (0..5)
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: Review::RATING }
+
+end
